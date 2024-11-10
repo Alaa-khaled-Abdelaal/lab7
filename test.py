3665fec7c1b89fa124a7573c570754e0d350425f
Alaa-khaled-Abdelaal <A.khaled2277@nu.edu.eg>
2024-11-10 16:10:56 +0200

    Create test.py

---
+++ test.py
@@ -0,0 +1,10 @@
+import unittest
+from addi import Calculator
+class Testcalculator(unittest.TestCase):
+def test_add(self):
+    result = Calculator.add(3, 7)
+    self.assertEqual(result, 10)
+    result= Calculator.add(-1, 1)
+    self.assertEqual(result, 0)
+    result = Calculator.add(-1, -1)
+    self.assertEqual(result, -2)
